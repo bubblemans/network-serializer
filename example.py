ed3dd661330e06fd0de4bbfaa03b465b65365412
Thomas.Callaghan <47304742+ThCallaghan@users.noreply.github.com>
2021-03-07 18:30:30 -0800

    Add alt_encode() to implement bits w/ 'u' char

--- example.py
+++ example.py
@@ -16,8 +16,10 @@ if __name__ == '__main__':
 
     # encode
     header = Serializer(fmt='!HHHHHH', encoding='dec', id=17, flags=1 << 8, QDCOUNT=1, ANCOUNT=0, NSCOUNT=0, ARCOUNT=0).encode()
+    header_alt = Serializer(fmt='!Hu4uuuuu3u4uHHHH', encoding='dec', id=17, QUERY=0, OPCODE=0, AA=0, TRUNCATION=0, RECURSION_DESIRED=1, RECURSION_AVAILABLE=0, Z=0, RCODE=0, QDCOUNT=1, ANCOUNT=0, NSCOUNT=0, ARCOUNT=0).alt_encode()
     question_name = Serializer(fmt='!BtBt', encoding='dec', ch_length_1=6, domain_name_1='google', ch_length_2=3, domain_name_2='com').encode()
     question_type = Serializer(fmt='!H', encoding='dec', Q_TYPE=1).encode()
     question_class = Serializer(fmt='!H', encoding='dec', Q_CLASS=1).encode()
     query = question_name + question_type + question_class
     print(header + query)
+    print(header_alt + query)
